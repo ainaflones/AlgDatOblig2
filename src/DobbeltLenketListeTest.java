e8cfe304f4189ce1e543a1ade48397501e12850f
Roy Hung <43883872+arroy114@users.noreply.github.com>
2019-10-07 13:24:07 +0200

    oppdatert oppgave6

--- src/DobbeltLenketListeTest.java
+++ src/DobbeltLenketListeTest.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -914,6 +915,9 @@ class DobbeltLenketListeTest {
 
         long maks = Math.max(tid1, tid2);
         long min = Math.min(tid1, tid2);
+
+        System.out.println(tid1);
+        System.out.println(tid2);
 
         if (maks > 1.5 * min) {
             System.out.println("Oppgave 6zg: Ineffektiv kode! Har du kodet den ene fjern-metoden");
